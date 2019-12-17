cb5db0706f9c8b4adf8871facdbce9ce01454cb8
MetroEzyCab <tabishskhn@gmail.com>
2019-12-17 18:03:03 +0530

    Shop N Trips

--- app/src/main/java/com/imuons/shopntrips/views/DashboardActivity.java
+++ app/src/main/java/com/imuons/shopntrips/views/DashboardActivity.java
@@ -4,21 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-
+import androidx.appcompat.widget.Toolbar;
 import com.imuons.shopntrips.R;
 import com.imuons.shopntrips.fragments.DashboardFragment;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DashboardActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager;
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ButterKnife.bind(this);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.title_activity_dashboard);
+        }
+
         fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, DashboardFragment.newInstance()).commit();
     }
