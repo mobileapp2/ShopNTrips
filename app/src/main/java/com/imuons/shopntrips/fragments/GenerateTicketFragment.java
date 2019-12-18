314235996962726e797ee5a8fed1eaee95777867
MetroEzyCab <tabishskhn@gmail.com>
2019-12-18 18:12:53 +0530

    Merge branch 'master' of https://github.com/tabbykhan/ShopNTrips
    # Conflicts:
    #	app/src/main/java/com/imuons/shopntrips/retrofit/ShopNTrips.java

--- app/src/main/java/com/imuons/shopntrips/fragments/GenerateTicketFragment.java
+++ app/src/main/java/com/imuons/shopntrips/fragments/GenerateTicketFragment.java
@@ -54,17 +54,52 @@ public class GenerateTicketFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_generate_ticket, container, false);
         ButterKnife.bind(this, view);
+        // getDepartments();
+
         return view;
     }
+
     @Override
     public void onResume() {
         super.onResume();
         if (Utils.checkInternetConnection(GenerateTicketFragment.this.getContext())) {
-            
+            // getDepartments();
         } else {
             Toast.makeText(GenerateTicketFragment.this.getContext(),
                     getString(R.string.no_internet_connection_message), Toast.LENGTH_SHORT).show();
         }
     }
+
+   /* private void getDepartments() {
+        final ProgressDialog pd = ViewUtils.getProgressBar(GenerateTicketFragment.this.getContext(), "Loading...", "Please wait..!");
+
+        ShopNTrips apiService = ApiHandler.getApiService();
+        final Call<DepartmentResponseModel> loginCall = apiService.wsGetDepartments("Bearer " + SharedPreferenceUtils.getLoginObject(
+                GenerateTicketFragment.this.getContext()).get().getAccess_token());
+        loginCall.enqueue(new Callback<DepartmentResponseModel>() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onResponse(Call<DepartmentResponseModel> call,
+                                   Response<DepartmentResponseModel> response) {
+                pd.hide();
+                if (response.isSuccessful()) {
+                    DepartmentResponseModel DepartmentResponseModel = response.body();
+                    if (DepartmentResponseModel.getCode() == Constants.RESPONSE_CODE_OK &&
+                            DepartmentResponseModel.getStatus().equals("OK")) {
+                     //   setSpinnerAdapter(DepartmentResponseModel.getData());
+                    } else {
+                        Toast.makeText(GenerateTicketFragment.this.getContext(), DepartmentResponseModel.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DepartmentResponseModel> call,
+                                  Throwable t) {
+                pd.hide();
+                Toast.makeText(SupportFragment.this.getContext(), getString(R.string.something_went_wrong), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }*/
 
 }
