a127a04aee9303f5b22c695a439f847606722ce5
admin <imuonswebsolutionpvtltd@gmail.com>
2019-12-19 16:25:08 +0530

    Registarion

---
+++ app/src/main/java/com/imuons/shopntrips/model/FundRequestReportResponseModel.java
@@ -0,0 +1,51 @@
+package com.imuons.shopntrips.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class FundRequestReportResponseModel {
+    @SerializedName("code")
+    @Expose
+    private Integer code;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("data")
+    @Expose
+    private FundRequestReportDataModel data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public FundRequestReportDataModel getData() {
+        return data;
+    }
+
+    public void setData(FundRequestReportDataModel data) {
+        this.data = data;
+    }
+}
