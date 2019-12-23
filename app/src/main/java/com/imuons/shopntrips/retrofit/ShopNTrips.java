24de2909ea3ab3dd94e6a76d0cd3488ba596b8d7
admin <imuonswebsolutionpvtltd@gmail.com>
2019-12-23 09:48:46 +0530

    Registarion

--- app/src/main/java/com/imuons/shopntrips/retrofit/ShopNTrips.java
+++ app/src/main/java/com/imuons/shopntrips/retrofit/ShopNTrips.java
@@ -8,6 +8,7 @@ import com.imuons.shopntrips.model.CheckUserExistResponseModel;
 import com.imuons.shopntrips.model.DashboardResponseModel;
 import com.imuons.shopntrips.model.DepartmentResponseModel;
 import com.imuons.shopntrips.model.DirectRoiReportResponseModel;
+import com.imuons.shopntrips.model.DownlineSummaryResponseModel;
 import com.imuons.shopntrips.model.DownlineTopUpReportResponseModel;
 import com.imuons.shopntrips.model.FundRequestReportResponseModel;
 import com.imuons.shopntrips.model.FundRequestResponseModel;
@@ -184,6 +185,9 @@ public interface ShopNTrips {
   @FormUrlEncoded
    @POST("get-user-first-topup")
     Call<UserTopUpResponse> wsTopUP(@Header("Authorization") String authHeader, @FieldMap Map<String, String> loginMap);
+
+    @GET("downline-summary")
+    Call<DownlineSummaryResponseModel> wsDownlineSummary(@Header("Authorization") String authHeader);
   /*
 
 
